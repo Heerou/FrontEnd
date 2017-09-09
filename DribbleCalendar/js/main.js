eef2a4f59b78b30cdfa1c6fb59c26d027e32b711
Julio Alvarez Pimiento <jalvarezpimiento@gmail.com>
2017-09-09 10:46:40 -0500

    Calendar part 1 complete

--- DribbleCalendar/js/main.js
+++ DribbleCalendar/js/main.js
@@ -1,5 +1,31 @@
 $(function() {
     $('#calendar-section').fullCalendar({
 
+        firstDay: 1,
+        aspectRatio: 1.3, 
+        dragScroll: false,
+        
+        header: {
+            left:   'prev',
+            center: 'title',
+            right:  'next'
+        },
+
+        monthNames: [
+            'Ene', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dic'
+        ],
+
+        dayNamesShort: [
+            'Dom', 'Lun', 'Mar', 'Mie',
+            'Jue', 'Vie', 'Sab'
+        ],
+
+        dayRender: function (date, cell) {
+            if(date.format('YYYY-MM-DD') == '2017-09-30') {
+                cell.addClass('with-event');
+            }
+        },
+
     });
 });
